a0bfc4ee7c7fd025e839c583c92626ddddc5274f
Aman Shukla <amanshukla9817@gmail.com>
2025-12-07 22:43:02 +0530

    FindMin

--- Array/FindMin.java
+++ Array/FindMin.java
@@ -2,7 +2,7 @@ package Array;
 
 public class FindMin {
     public static void main(String[] args) {
-        int[] arr = {18,12,-7,3,14,28};
+        int[] arr = {18,12,7,3,14,28};
         System.out.println(min(arr));
     }
     // assume arr.length != 0
